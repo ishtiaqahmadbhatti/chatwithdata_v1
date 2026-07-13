3ccf88667b44494402cbc33c47e92efb5e7795a2
ishtiaqahmadbhatti <ishtiaq.uaar55@gmail.com>
2026-07-13 23:36:31 +0500

    LangSmith env Setup

--- chatwithdata.api/app/app_core/config.py
+++ chatwithdata.api/app/app_core/config.py
@@ -1,6 +1,10 @@
 from pydantic_settings import BaseSettings
 from typing import Optional
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file into os.environ
+load_dotenv()
 
 
 class Settings(BaseSettings):
